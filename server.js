e00453ebbbc0d03db1dbfcd9bd15ea098c7146c4
minhha <hoangminhha2411@gmail.com>
2022-07-04 17:36:35 +0700

    adđ user, auth, profile, posts route, export all route to app.use()

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const PORT = process.env.port || 3000;
 // Connect to DB
 connectDB();
 
+// Define Routes
+
+app.use("/api/users", require("./routers/api/users"));
+app.use("/api/posts", require("./routers/api/posts"));
+app.use("/api/profile", require("./routers/api/profile"));
+app.use("/api/auth", require("./routers/api/auth"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
